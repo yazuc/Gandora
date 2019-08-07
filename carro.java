f9a8255db2fdc4edcabbe9608c4c43e40dc367d0
empucrs <51421368+empucrs@users.noreply.github.com>
2019-08-07 01:22:11 -0300

    Create carro.java

---
+++ carro.java
@@ -0,0 +1,46 @@
+public class carro {
+	
+	private float quilometragem;
+	private float tanque, capacidade;
+	
+	public Carro(int cpde) {
+		capacidade=cpde;
+	}
+	
+	public float getQuilometragem() {
+		return quilometragem;
+	}
+	public float getCapacidadeTanque() {
+		return capacidade;
+	}
+	public float getCombustivelRestante() {
+		return tanque;
+	}
+	public float getOcupacaoTanque() {
+		return (tanque/capacidade)*100;
+	}
+	public void abastercer(float qtde) {
+		if(tanque+qtde<=capacidade)
+			tanque+=qtde;
+		else
+			tanque=capacidade;
+	}
+	public float deslocar(float kms) {
+		
+		float consumo = (float)(Math.random()*5.0+10); // consumo máximo 15km/l == mínimo 10
+		float combustivelNecessario = kms/consumo;
+		System.out.println("  Desempenho do deslocamento " + consumo + "km/l");
+		if(combustivelNecessario>tanque) {
+			tanque=0;
+		    float deslocou=tanque/consumo;
+		    quilometragem+=deslocou;
+		    return deslocou;
+		}
+		else {
+			tanque-=combustivelNecessario;
+		    quilometragem+=kms;
+		    return kms;
+		}
+	}
+	
+}
